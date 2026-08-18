80298be1bea041b959b74e3cdaa93df53f111cca
Apollo Nargang <CoffeeCoder1@outlook.com>
2026-08-17 20:09:49 -0400

    Remove sample feedbacks
    The sample feedback shouldn't be in the final package, and also I don't
    really think this box would be served well by any feedbacks, since
    everything is just true/false values for the most part (which fit well
    with variables), so I removed kinda the whole framework for feedbacks.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { GetConfigFields, type ModuleConfig } from './config.js'
 import { UpdateVariableDefinitions, type VariablesSchema } from './variables.js'
 import { UpgradeScripts } from './upgrades.js'
 import { UpdateActions, type ActionsSchema } from './actions.js'
-import { UpdateFeedbacks, type FeedbacksSchema } from './feedbacks.js'
 import { UpdatePresets } from './presets.js'
 
 export type ModuleSchema = {
 	config: ModuleConfig
 	secrets: undefined
 	actions: ActionsSchema
-	feedbacks: FeedbacksSchema
+	feedbacks: Record<string, never>
 	variables: VariablesSchema
 }
 
@@ -32,7 +31,6 @@ export default class ModuleInstance extends InstanceBase<ModuleSchema> {
 		this.updateStatus(InstanceStatus.Ok)
 
 		this.updateActions() // export actions
-		this.updateFeedbacks() // export feedbacks
 		this.updatePresets() // export Presets
 		this.updateVariableDefinitions() // export variable definitions
 
@@ -60,10 +58,6 @@ export default class ModuleInstance extends InstanceBase<ModuleSchema> {
 
 	updateActions(): void {
 		UpdateActions(this)
-	}
-
-	updateFeedbacks(): void {
-		UpdateFeedbacks(this)
 	}
 
 	updatePresets(): void {
